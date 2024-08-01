992e94ebcea4f5e603cdb1691d1955bd7e09435b
Gabriel Hartmann <162393857+GabHartmann@users.noreply.github.com>
2024-08-01 15:14:55 -0300

    Progress

--- JStoExpert/JS/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
+++ JStoExpert/JS/complete-javascript-course-master/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignments.js
@@ -1,12 +1,12 @@
 const country = "Brazil";
 const continet = "South America"
-let population = 200000000000;
-
+let population = 200;
+let isIsland = false;
 console.log(country)
 console.log(continet)
 console.log(population)
 
-let language = "Portuguese"
+let language = "not"
 
 console.log(typeof isIsland);
 console.log(typeof continet)
@@ -29,4 +29,40 @@ let description = 'Brazil is in South America, and 20000000 people speak portugu
 
 
 
+if (population > 33) {
+    console.log(`${country}'s population is ${population - 33} million above average`);
+} else {
+    console.log(`${country}'s population is ${33 - population} million below average`)
+}
 
+
+
+console.log(population < 55 && language === 'English' && !isIsland ? console.log("You should live in Portugal :)") : console.log("Portugal does not meet your creteria :("));
+
+
+
+switch (language) {
+    case 'Chinese':
+    case 'Mandarin':
+        console.log("Most number of native speaker!");
+        break;
+    case 'Spanish':
+        console.log("2nd place in numbers of native speakers!");
+        break;
+    case 'English':
+        console.log("3rd place");
+        break;
+    case 'Hindi':
+        console.log("4th place");
+        break;
+    case 'Arabic':
+        console.log("5h most spoken languase!");
+        break;
+    default:
+        console.log("Great language too!")
+}
+
+
+console.log(
+    `${country} puplation is ${population > 33 ? 'above' : 'below'} average.`
+);
