533412f58b3f7f6e34adc59b41a54dfbd76bb4fd
Gabriel Hartmann <162393857+GabHartmann@users.noreply.github.com>
2024-10-02 16:50:40 -0300

    First project classes created

--- complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector(".message").textContent = "Correct Numer!";
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 4;
+// document.querySelector('.guess').value;
+// document.querySelector('.guess').value = 5;
+
+document.querySelector('.check').addEventListener('click', function () {
+    console.log(document.querySelector('.guess').value);
+});
